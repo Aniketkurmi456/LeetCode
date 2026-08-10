d2971ee830e1c487532b4f68e82b3d84a27039b2
Aniket Kurmi <aniketkurmi456@gmail.com>
2026-08-10 21:59:38 +0530

    Solve LeetCode 1343 Number of Sub-arrays

---
+++ 1343_Number_of_Sub-arrays_of_Size_K_and_Average_Greater_than_or_Equal_to_Threshold/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int n = arr.size();
+        int avg;
+        int sum =0;
+        int i =0;
+        int j=0;
+        int count =0;
+        while(j<n){  
+            sum +=arr[j];
+            if (j-i+1==k){
+                if(sum>=k*threshold){
+                    count ++;
+                }
+                sum-=arr[i];
+                i++;
+            }
+            j++;
+            
+        } 
+        return count;
+    }
+};
