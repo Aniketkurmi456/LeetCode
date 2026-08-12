c0205be7b56bea1bf38b8faf6d95a16d258640b0
Aniket Kurmi <aniketkurmi456@gmail.com>
2026-08-12 13:06:41 +0530

    Solve LeetCode 219 Contains Duplicate II

---
+++ 0219_Contains_Duplicate_II/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> st;
+
+        int i = 0;
+        int j = 0;
+
+        while (j < nums.size()) {
+            if (st.find(nums[j]) != st.end()) {
+                return true;
+            }
+
+            st.insert(nums[j]);
+
+            if (j - i >= k) {
+                st.erase(nums[i]);
+                i++;
+            }
+
+            j++;
+        }
+
+        return false;
+    }
+};
